f866eb828cd9140648a55afff60f13753de9a893
Rakib Hossen <rakibhossen040@gmail.com>
2023-10-28 11:41:59 +0600

    added create ticket page and initiate the faq pages

---
+++ src/pages/GetHelps/FAQ/FAQ.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Profile from "../../../components/common/Profile";
+
+const FAQ = () => {
+  return (
+    <div>
+      <Profile />
+      <h1>Frequently Asked Questions</h1>
+    </div>
+  );
+};
+
+export default FAQ;
