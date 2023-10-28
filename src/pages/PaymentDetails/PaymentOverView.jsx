79dffa2f86789d76896110940a9d0b12148b3f0e
Shipon Hossen Raju <msshipon234@gmail.com>
2023-10-28 13:26:14 +0600

    Payment overview complete

--- src/pages/PaymentDetails/PaymentOverView.jsx
+++ src/pages/PaymentDetails/PaymentOverView.jsx
@@ -1,7 +1,138 @@
-import React from "react";
+import { Grid, _ } from "gridjs-react";
+import { useState } from "react";
 
 const PaymentOverView = () => {
-  return <div>PaymentOverView</div>;
+  const [originalData, setOriginalData] = useState([
+    {
+      createDate: "Tue, 24 Oct 2023 12:57:16 +0000",
+      trustAmount: "$650",
+      status: "Returned",
+      paymentAmount: "$650",
+      _id: "26874ysatdhfkajsl",
+    },
+    {
+      createDate: "Tue, 24 Oct 2023 01:16:09 +0000",
+      trustAmount: "$450",
+      status: "Pending",
+      paymentAmount: "$450",
+      _id: "26874ysasdhfkajsl",
+    },
+    {
+      createDate: "Mon, 23 Oct 2023 23:35:38 +0000",
+      trustAmount: "$500",
+      status: "paid",
+      paymentAmount: "$500",
+      _id: "26874ysadhfdkajsl",
+    },
+    {
+      createDate: "Tue, 24 Oct 2023 12:57:16 +0000",
+      trustAmount: "$650",
+      status: "Returned",
+      paymentAmount: "$650",
+      _id: "26874ysatdhfkajsl",
+    },
+    {
+      createDate: "Tue, 24 Oct 2023 01:16:09 +0000",
+      trustAmount: "$450",
+      status: "Pending",
+      paymentAmount: "$450",
+      _id: "26874ysasdhfkajsl",
+    },
+    {
+      createDate: "Mon, 23 Oct 2023 23:35:38 +0000",
+      trustAmount: "$500",
+      status: "paid",
+      paymentAmount: "$500",
+      _id: "26874ysadhfdkajsl",
+    },
+  ]);
+
+  return (
+    <div
+      className="mt-6 border-[#3048AD] p-[1px] rounded-2xl w-full"
+      // style={{
+      //   background: `linear-gradient(to right, #2d2d47, #3048AD) `,
+      // }}
+    >
+      <div className="h-full rounded-2xl py-4 px-8 max-w-full">
+        {originalData.length ? (
+          <div className="relative">
+            <div className="relative flex justify-between mb-3 mt-1    w-full flex-col lg:flex-row bg-transparent rounded overflow-x-auto">
+              {originalData.length ? (
+                <Grid
+                  data={originalData.map((x) => [
+                    x.createDate,
+                    x.trustAmount,
+                    x.status,
+                    x.paymentAmount,
+                    x._id,
+                  ])}
+                  columns={[
+                    {
+                      name: "Created Date",
+                      formatter: (cell) => {
+                        const date = new Date(cell);
+                        const day = date.getDate();
+                        const month = date.getMonth() + 1;
+                        const year = date.getFullYear();
+
+                        return `${month}/${day}/${year}`;
+                      },
+                    },
+                    {
+                      name: "Trust Amount",
+                    },
+                    {
+                      name: "Status",
+                      formatter: (cell) => {
+                        return _(
+                          <div className="inline-flex justify-center items-center">
+                            <span
+                              className={`py-1 px-2.5 rounded-[0.5rem] ${
+                                cell == "paid"
+                                  ? "bg-[#064E3B] text-[#ECFDF5]"
+                                  : cell == "Pending"
+                                  ? "bg-[#D7D7FF] text-[#090858]"
+                                  : cell == "Returned"
+                                  ? "text-[#991B1B] bg-[#FEF2F2]"
+                                  : ""
+                              }`}
+                            >
+                              {cell}
+                            </span>
+                          </div>
+                        );
+                      },
+                    },
+                    {
+                      name: "Trust Amount",
+                    },
+                  ]}
+                  // search={false}
+                  // pagination={{
+                  //   limit: 10,
+                  // }}
+                  className={{
+                    container: "",
+                    table:
+                      "items-center bg-transparent min-w-[80vw] lg:min-w-[70vw] border-collapse  font-Ar mt-6 mb-8 table-auto",
+                    th: "px-2 bg-blueGray-50 text-blueGray-500 align-middle py-3 text-base whitespace-nowrap font-semibold text-center",
+                    td: "border-t-0 px-1 md:px-2  border-l-0 border-r-0 text-base whitespace-nowrap p-2 md:p-4  text-center text-[#64748B] border-b border-[#E7EAEE]",
+                    paginationSummary: "hidden md:block",
+                    paginationButton: "border px-3 py-1 me-3 ",
+                    paginationButtonCurrent:
+                      "bg-[#3637EA] px-3 py-1 border-[#3637EA] ",
+                    paginationButtonNext: "border-none",
+                    paginationButtonPrev: "border-none",
+                  }}
+                />
+              ) : null}
+            </div>
+          </div>
+        ) : null}
+      </div>
+    </div>
+  );
 };
 
 export default PaymentOverView;
--- src/routes/Index.jsx
+++ src/routes/Index.jsx
@@ -7,7 +7,6 @@ import Dashboard from "../pages/Dashboard/Dashboard";
 import PaymentDetails from "../pages/PaymentDetails/PaymentDetails";
 import Settings from "../pages/Settings/Settings";
 import AuthProtected from "./AuthProtected";
-import PaymentOverview from "../pages/PaymentOverview/PaymentOverview";
 
 const router = createBrowserRouter([
   {
@@ -32,17 +31,12 @@ const router = createBrowserRouter([
         element: <AccountLinks />,
       },
       {
-<<<<<<< HEAD
-        path: "/payment-overview",
-        element: <PaymentOverview />,
-=======
         path: "/payment-details",
         element: <PaymentDetails />,
       },
       {
         path: "/setting",
         element: <Settings />,
->>>>>>> c641a4ac60ca8cb8c5d6b5391e7accead1a519e3
       },
       {
         path: "*",
